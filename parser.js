60c1f3fc6ed580c11dbc98742b51adda8de4a7f3
niranjana687 <niranjana687@gmail.com>
2021-08-14 21:08:45 +0530

    implemented parser

--- parser.js
+++ parser.js
@@ -3,12 +3,70 @@ import { lexer } from './lexer.js';
 //returns abstract syntax tree
 
 const parser = (tokens) => {
-    while(tokens.length > 0) {
-        let currentTokenIndex = 0;
+     let current = 0;
 
-        const parseNumber = () => ({
-            value: parseInt(tokens[currentTokenIndex++]), 
-            type: 'number',
-        });
-    }
+     function walk() {
+
+        let token = tokens[current];
+
+        //checks for number token, returns NumberLiteral AST if true.
+        if (token.type === 'number') {
+
+            current++;
+
+            return {
+                type: 'NumberLiteral',
+                value: token.value,
+            };
+        }
+
+        //String literal
+        if (token.type === 'string') {
+
+            current++;
+
+            return {
+                type: 'StringLiteral',
+                value: token.value,
+            };
+
+        }
+
+        // add ( 2 subtract ( 4 2))
+        if (token.type === 'paren' && token.value === '(') {
+            token = tokens[++current];
+
+            let node = {
+                type: 'CallExpression',
+                name: token.value,
+                params: [],
+            };
+
+            token = tokens[++current];
+
+            while ( token.type !== 'paren' || (token.type === 'paren' && token.value !== ')')) {
+
+                node.params.push(walk());
+                token = tokens[++current];
+
+            }
+
+            current++;
+
+            return node;
+        }
+
+        throw new TypeError(token.type);
+     }
+
+     let ast = {
+         type: 'Program',
+         body: [],
+     };
+
+     while (current < tokens.length) {
+         ast.body.push(walk());
+     }
+
+     return ast;
 }
