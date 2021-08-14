41e6055d918e1ac85595520e333d932a54d6c50d
niranjana687 <niranjana687@gmail.com>
2021-08-14 16:27:34 +0530

    implemented lexical analyser

--- lexer.js
+++ lexer.js
@@ -1,14 +1,105 @@
-const lexer = (code) => {
-    return code.split(/\s+/)
-               .filter(function (t) {  return t.length > 0})
-               .map( function (t) {
-                   return isNaN(t) 
-                            ? {type: 'word', value:t}
-                            : {type: 'number', value: t}
-               })
-}
+const lexer = (input) => {
 
-// console.log(lexer('6 + 4'));
+    //Tracks the postion in the code like a cursor
+    let current = 0;
+
+    //For pushing tokens into
+    let tokens = [];
+
+    while (input < current.length) {
+
+        let char = input[current];
+
+        //check for open parenthesis.
+        if (char === '(') {
+            tokens.push({
+                type: 'paren',
+                value: '(',
+            });
+            
+            current++;
+            continue;
+        }
+
+        //check for close parenthesis
+        if (char === ')') {
+            tokens.push({
+                type: 'paren',
+                value: ')'
+            });
+
+            current++;
+            continue;
+        }
+
+        //whitespace check since it aids in identifying tokens.
+        let WHITESPACE = /\s/;
+        if (WHITESPACE.test(char)) {
+            current++;
+            continue;
+        }
+
+        //check for numbers
+        let NUMBERS = /[0-9]/;
+        if (NUMBERS.test(char)) {
+            let value = '';
+
+            while(NUMBERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+
+            tokens.push({
+                type: 'number',
+                value: value,
+            });
+
+            continue;
+        }
+
+        // strings
+        if (char === '"') {
+
+            let value = '';
+
+            char = input[++current];
+
+            while (char !== '"') {
+                value += char;
+                char = input[++current];
+            }
+
+            tokens.push({ 
+                type: 'string',
+                value: value,
+            });
+
+            continue;
+        }
+
+        //checks for names like 'add', 'sub', 'mul' and 'div'
+        let LETTERS = /[a-z]/i;
+
+        if (LETTERS.test(char)) {
+            let value = '';
+
+            while (LETTERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+
+            tokens.push({
+                type: 'name',
+                value: value,
+            }); 
+        }
+
+        throw new TypeError('Tiny Compiler does not understand this character' + char);
+        
+    }
+    return tokens;
+};
+
 exports.module = {
     lexer,
 };
--- parser.js
+++ parser.js
@@ -1,3 +1,14 @@
 import { lexer } from './lexer.js';
 
-//returns parse tree 
+//returns abstract syntax tree
+
+const parser = (tokens) => {
+    while(tokens.length > 0) {
+        let currentTokenIndex = 0;
+
+        const parseNumber = () => ({
+            value: parseInt(tokens[currentTokenIndex++]), 
+            type: 'number',
+        });
+    }
+}
