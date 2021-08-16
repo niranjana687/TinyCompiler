ad24d0d40798ef2baf2d0cff1e6476daa4598eae
niranjana687 <niranjana687@gmail.com>
2021-08-16 08:50:30 +0530

    transformer completed

---
+++ transformer.js
@@ -0,0 +1,73 @@
+import { traverser } from './traverser.js';
+
+const transformer = (ast) => {
+
+    //to store transformed ast.
+    let newAst = {
+        type: 'Program',
+        body: [],
+      };
+    
+    
+      ast._context = newAst.body;
+      
+      traverser(ast, {
+    
+       
+        NumberLiteral: {
+         
+          enter(node, parent) {
+            
+            parent._context.push({
+              type: 'NumberLiteral',
+              value: node.value,
+            });
+          },
+        },
+    
+       
+        StringLiteral: {
+          enter(node, parent) {
+            parent._context.push({
+              type: 'StringLiteral',
+              value: node.value,
+            });
+          },
+        },
+    
+
+        CallExpression: {
+          enter(node, parent) {
+    
+            
+            let expression = {
+              type: 'CallExpression',
+              callee: {
+                type: 'Identifier',
+                name: node.name,
+              },
+              arguments: [],
+            };
+    
+            
+            node._context = expression.arguments;
+    
+            
+            if (parent.type !== 'CallExpression') {
+    
+     
+              expression = {
+                type: 'ExpressionStatement',
+                expression: expression,
+              };
+            }
+    
+           
+            parent._context.push(expression);
+          },
+        }
+      });
+    
+     
+      return newAst;
+}
